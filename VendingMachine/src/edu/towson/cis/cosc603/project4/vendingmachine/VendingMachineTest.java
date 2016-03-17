6008b221cf447af8a8ab36a727849acf973b349b
jrehakTow <jrehak1@students.towson.edu>
2016-03-17 15:49:51 -0400

    T4- Add removeItem test cases

--- VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
@@ -43,6 +43,7 @@ public class VendingMachineTest {
 	public void testAddItem() {
 		//add item normal. 
 		sodaPopMachine.addItem(jolt, "D");
+		assertEquals(jolt, sodaPopMachine.getItem("D"));
 		
 	}
 	
@@ -50,12 +51,20 @@ public class VendingMachineTest {
 	public void testAddItem1BadCode() {
 		//add item wrong code
 		sodaPopMachine.addItem(pesi, "E");
+		assertNotEquals(pesi, sodaPopMachine.getItem("E"));
 	}
 	
 	@Test(expected = VendingMachineException.class)
 	public void testAddItem2SlotOccupied() {
 		//add too many times
-		sodaPopMachine.addItem(coke, "A");	
+		sodaPopMachine.addItem(jolt, "A");	
+		assertEquals(coke, sodaPopMachine.getItem("A"));
+	}
+	
+	@Test(expected = VendingMachineException.class)
+	public void testAddItem3LowerCaseCode() {
+		sodaPopMachine.addItem(jolt, "d");	
+		assertNotEquals(jolt, sodaPopMachine.getItem("D"));
 	}
 
 	@Test
@@ -68,10 +77,36 @@ public class VendingMachineTest {
 		sodaPopMachine.removeItem("A");
 		assertNotEquals(coke, sodaPopMachine.removeItem("A")); 
 	}
+	
+	@Test
+	public void testRemoveItem2AllGone() {
+		/*
+		sodaPopMachine.addItem(jolt, "D");
+		sodaPopMachine.removeItem("A");
+		sodaPopMachine.removeItem("B");
+		sodaPopMachine.removeItem("C");
+		sodaPopMachine.removeItem("D");
+		*/
+		//sodaPopMachine.addItem(coke, "A");
+		sodaPopMachine.addItem(jolt, "D");
+		
+		String[] code = {"A", "B", "C", "D"};
+		VendingMachineItem[] soda = {coke, pesi, drPepper, jolt};
+		for(int i = 0; i<4; i++){
+			sodaPopMachine.removeItem(code[i]);
+			assertNotEquals(soda[i], sodaPopMachine.getItem(code[i]));
+		}
+		//assertNotEquals(coke, sodaPopMachine.removeItem("A")); 
+	}
+	
+	@Test(expected = VendingMachineException.class)
+	public void testRemoveItem3BadInput() {
+		sodaPopMachine.removeItem("E");
+	}
 
 	@Test
 	public void testInsertMoney() {
-		sodaPopMachine.insertMoney(2);
+		sodaPopMachine.insertMoney(2.00);
 		assertEquals(2.00, sodaPopMachine.balance, 0.0);
 	}
 	
@@ -80,6 +115,12 @@ public class VendingMachineTest {
 		sodaPopMachine.balance = 1.00;
 		sodaPopMachine.insertMoney(2);
 		assertEquals(3.00, sodaPopMachine.balance, 0.0);
+	}
+	
+	@Test(expected = VendingMachineException.class)
+	public void testInsertMoney2NegativeINput() {
+		sodaPopMachine.insertMoney(-1);
+		assertEquals(0, sodaPopMachine.balance, 0.0);
 	}
 
 	@Test
@@ -110,10 +151,19 @@ public class VendingMachineTest {
 		assertFalse(sodaPopMachine.makePurchase("D"));	
 	}
 	
+	
 	@Test(expected = VendingMachineException.class)
-	public void testMakePurchase2BadInput() {
+	public void testMakePurchase3BadInput() {
 		//failure bad code
 		assertFalse(sodaPopMachine.makePurchase("E"));
+	}
+	
+	@Test
+	public void testMakePurchase4HigherBalance() {
+		//failure out of stock
+		sodaPopMachine.balance = 2.00;
+		assertTrue(sodaPopMachine.makePurchase("A"));
+		assertEquals(0.50, sodaPopMachine.balance, 0.0);
 	}
 
 	@Test
@@ -125,7 +175,7 @@ public class VendingMachineTest {
 	}
 	
 	@Test
-	public void testReturnChange1() {
+	public void testReturnChange1NoChange() {
 		sodaPopMachine.balance = 0.0;
 		assertEquals(0.0, sodaPopMachine.returnChange(), 0.0);
 	}
