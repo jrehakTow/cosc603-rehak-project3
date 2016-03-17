c5da81633c1efd4cf51e5c442bf6710213f57913
jrehakTow <jrehak1@students.towson.edu>
2016-03-17 00:41:52 -0400

    T4- Add Test cases to VendingMachineItem
    Cover all methods and less than 0 case.

--- VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineItemTest.java
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineItemTest.java
@@ -7,9 +7,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineItemTest {
+	
+	VendingMachineItem coke;
+	VendingMachineItem pesi;
 
 	@Before
 	public void setUp() throws Exception {
+		coke = new VendingMachineItem("coke", 1.50);
 	}
 
 	@After
@@ -18,77 +22,25 @@ public class VendingMachineItemTest {
 
 	@Test
 	public void testVendingMachineItem() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		//test normal
+		assertNotNull(coke);
+	}
+	
+	@Test(expected = VendingMachineException.class)
+	public void testVendingMachineItemLessThanZero() {
+		pesi = new VendingMachineItem("pesi", -1);
+		assertNull(pesi);
 	}
 
 	@Test
 	public void testGetName() {
-		fail("Not yet implemented");
+		assertEquals("coke", coke.getName());
 	}
 
 	@Test
 	public void testGetPrice() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testObject() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetClass() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHashCode() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEquals() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testClone() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNotify() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNotifyAll() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWaitLong() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWaitLongInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWait() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFinalize() {
-		fail("Not yet implemented");
+		assertEquals(1.50, coke.getPrice(), 0.0);
 	}
 
 }
